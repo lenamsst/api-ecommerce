e7f41053e56d89bf7a8a4571199cdd75b5b48eae
Milena <lenamsst@gmail.com>
2022-05-04 10:36:55 -0300

    feat: conexão com mongodb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log ('olá mundo')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}, console.log('Connected to database'))
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Olá Mundo')
+})
+
+app.listen(3333, () => console.log('Server running'))
